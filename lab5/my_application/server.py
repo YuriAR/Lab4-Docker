4efcac990be506da370d39780684795525d788e3
Yuri Reis <yrreis@gmail.com>
2015-10-14 17:40:37 +0300

    altering server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "Index File \n"
+
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    return "Hello world \n"
+
+@app.route("/user/<username>")
+def user_profile(username):
+    return "User %s \n" % username
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    return "Post %d \n" % post_id
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
